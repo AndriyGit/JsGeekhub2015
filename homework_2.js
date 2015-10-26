f22532b145b17d9a9d682b2ec73aa89b0d138551
Andrey Borovoy <andriy_borovuy@ukr.net>
2015-10-26 13:21:41 +0200

    Homework 2: Refactor script

--- homework_2.js
+++ homework_2.js
@@ -3,39 +3,32 @@
  */
 
 // to complete any level use the script bellow
-var oppositeSide = {'north' : 'south', 'east' : 'west', 'south' : 'north', 'west' : 'east'};
-var directions = Object.keys(oppositeSide);
-var levelComplete = false;
+var oppositeSideFor  = {'north' : 'south', 'east' : 'west', 'south' : 'north', 'west' : 'east'};
+var rightHandBasedOn = {'north' : 'east', 'east' : 'south', 'south' : 'west', 'west' : 'north'};
+var levelComplete = true;
 var rightHand, lastStep;
 
-function runForestRun() {
-  if(lastStep !== undefined) {
-    if(isFree(rightHand)) {
-      makeStepTo(rightHand);
-    } else if(isFree(lastStep)) {
-      makeStepTo(lastStep);
-    }else if(isFree(newDirection(lastStep))) {
-      rightHand = lastStep;
-      makeStepTo(newDirection(lastStep));
-    } else {
-      makeStepTo(oppositeSide[lastStep]);
-    }
-  } else {
-    for(var i = 0; i < 4; i++) {
-      if(isFree(directions[i])) {
-        makeStepTo(directions[i]);
-        break;
-      }
-    }
-  }
+if(levelComplete) initialize();
+
+while(!levelComplete) {
+  runForestRun();
 }
 
-function newDirection(landmark) {
-  if(landmark === 'north') {
-    return 'west';
+function initialize() {
+  levelComplete = false;
+  rightHand = 'west';
+  lastStep = 'south';
+}
+
+function runForestRun() {
+  if(isFree(rightHand)) {
+    makeStepTo(rightHand);
+  } else if(isFree(lastStep)) {
+    makeStepTo(lastStep);
+  } else if(isFree(oppositeSideFor[rightHand])) {
+    makeStepTo(oppositeSideFor[rightHand]);
   } else {
-    var index = directions.indexOf(landmark) - 1;
-    return directions[index];
+    makeStepTo(oppositeSideFor[lastStep]);
   }
 }
 
@@ -43,26 +36,7 @@ function makeStepTo(direction) {
   var result;
   lastStep = direction;
   result = window[direction]();
-  if(result === 'next' || result === 'end') levelComplete = true
-  updateRightHand(lastStep);
+  if(result === 'next' || result === 'end') levelComplete = true;
+  rightHand = rightHandBasedOn[lastStep];
   map();
-}
-
-function updateRightHand(lastStep) {
-  // if lastStep equal to last element in array we need to calculate 'rightHand' in another way
-  if (lastStep !== 'west') {
-    var index = directions.indexOf(lastStep) + 1;
-    rightHand = directions[index];
-  } else {
-    rightHand = 'north';
-  }
-}
-
-while(!levelComplete) {
-  runForestRun();
-}
-
-if(levelComplete) {
-  levelComplete = false;
-  rightHand = lastStep = undefined;
 }
