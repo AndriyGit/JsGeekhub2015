a41916eb052fa9d8358af586c546adeaa607bcea
Andriy <andriy_borovuy@ukr.net>
2015-11-10 00:35:27 +0200

    Homework 4: Complete

--- homework_4.js
+++ homework_4.js
@@ -2,26 +2,6 @@
  * Created by andrey on 08.11.15.
  */
 
-var arr = ['a', 'b', 'c', 'd'];
-var numbers = [1, 4, 9, 16, 9, 12];
-
-function addFive(el) {
-  return el + 5;
-}
-
-function sumOfTwoNumbers(a, b) {
-  return a + b;
-}
-
-function isBiggerThanFive(element) {
-  console.log(element);
-  return element > 5;
-}
-
-function addSymbol(str) {
-  return str + 'q';
-}
-
 function mapCustom(array, func) {
   var i, currentValue;
   var newArray = [];
@@ -31,9 +11,6 @@ function mapCustom(array, func) {
   }
   return newArray;
 }
-
-//console.log(mapCustom(numbers, addFive));
-
 
 function reduceCustom(array, func, initValue) {
   var previousValue, currentValue, i;
@@ -53,10 +30,6 @@ function reduceCustom(array, func, initValue) {
   return previousValue;
 }
 
-//console.log(reduceCustom(numbers, sumOfTwoNumbers));
-//console.log(reduceCustom(numbers, sumOfTwoNumbers, 50));
-
-
 function forEachCustom(array, func) {
   var currentValue, i;
   for(i = 0; i < array.length; i++) {
@@ -64,15 +37,6 @@ function forEachCustom(array, func) {
     if(currentValue !== undefined) func(currentValue, i, array);
   }
 }
-
-/*
- custom forEach
-console.log(numbers);
-forEachCustom(numbers, function(el) {
-  console.log(el);
-});
-*/
-
 
 function someCustom(array, func) {
   var currentValue, i, result;
@@ -84,9 +48,6 @@ function someCustom(array, func) {
   return result;
 }
 
-//console.log(someCustom(numbers, isBiggerThanFive));
-
-
 function everyCustom(array, func) {
   var currentValue, i, result;
   for(i = 0; i < array.length; i++) {
@@ -96,10 +57,6 @@ function everyCustom(array, func) {
   }
   return result;
 }
-
-//console.log(everyCustom(numbers, isBiggerThanFive));
-//console.log(everyCustom(mapCustom(numbers, addFive), isBiggerThanFive));
-
 
 //Если fromIndex имеет отрицательное значение,
 //то поиск начинается с индекса, равного длине массива плюс fromIndex.
@@ -122,16 +79,77 @@ function indexOfCustom(array, searchElement, fromIndex) {
   return index;
 }
 
-//console.log(indexOfCustom(numbers, 9)); // expect 2
-//console.log(indexOfCustom(numbers, 9, -3)); // expect 4
-
-
-
-function reverseCustom(array, func) {
-
+function reverseCustom(array) {
+  var i = array.length - 1, j = 0;
+  var reversedArray = [];
+  for(i; i >= 0; i--) {
+    reversedArray[i] = array[j];
+    j++;
+  }
+  Object.assign(array, reversedArray);
+  return array;
 }
 
-function joinCustom(array, func) {
+function joinCustom(array, separator) {
+  var str = '', i;
+  if(separator === undefined) separator = ',';
 
+  //convert separator to string if it isn't string yet
+  if(typeof(separator) !== 'string') separator = '' + separator;
+
+  for(i = 0; i < array.length; i++) {
+    str += array[i];
+    if(i != array.length - 1) str += separator;
+  }
+  return str;
 }
 
+
+// here is additional data to help you check the functions above
+
+var numbers = [1, 4, 9, 16, 9, 12];
+
+function addFive(el) {
+  return el + 5;
+}
+
+function sumOfTwoNumbers(a, b) {
+  return a + b;
+}
+
+function isBiggerThanFive(element) {
+  console.log(element);
+  return element > 5;
+}
+
+//map
+console.log(mapCustom(numbers, addFive));
+
+//reduce
+console.log(reduceCustom(numbers, sumOfTwoNumbers));
+console.log(reduceCustom(numbers, sumOfTwoNumbers, 50));
+
+//forEach
+console.log(numbers);
+forEachCustom(numbers, function(el) {
+  console.log(el);
+});
+
+//some
+console.log(someCustom(numbers, isBiggerThanFive));
+
+//every
+console.log(everyCustom(numbers, isBiggerThanFive));
+console.log(everyCustom(mapCustom(numbers, addFive), isBiggerThanFive));
+
+//indexOf
+console.log(indexOfCustom(numbers, 9)); // expect 2
+console.log(indexOfCustom(numbers, 9, -3)); // expect 4
+
+//reverse
+console.log(numbers);
+console.log(reverseCustom(numbers));
+
+//join
+console.log(joinCustom(numbers));
+console.log(joinCustom(numbers, ' + '));
